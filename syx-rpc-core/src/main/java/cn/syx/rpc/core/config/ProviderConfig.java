57af2d3056e1ae2ccc480aaa72deb363d7ecb49e
HaydnSyx <HaydnSyx@163.com>
2024-04-12 21:57:23 +0800

    服务提供端增加apollo配置

--- syx-rpc-core/src/main/java/cn/syx/rpc/core/config/ProviderConfig.java
+++ syx-rpc-core/src/main/java/cn/syx/rpc/core/config/ProviderConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,6 +21,13 @@ public class ProviderConfig {
 
     @Value("${server.port:8080}")
     private int port;
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "apollo.bootstrap", value = "enabled")
+    ApolloChangedListener providerApolloChangedListener() {
+        return new ApolloChangedListener();
+    }
 
     @Bean
     public ProviderBootstrap providerBootstrap(
--- syx-rpc-demo-provider/src/main/java/cn/syx/rpc/demo/provider/SyxRpcDemoProviderApplication.java
+++ syx-rpc-demo-provider/src/main/java/cn/syx/rpc/demo/provider/SyxRpcDemoProviderApplication.java
@@ -2,15 +2,29 @@ package cn.syx.rpc.demo.provider;
 
 import cn.syx.rpc.core.annotation.EnableProvider;
 import cn.syx.rpc.core.config.ProviderConfig;
+import cn.syx.rpc.core.config.ProviderProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableProvider
 @SpringBootApplication
 public class SyxRpcDemoProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SyxRpcDemoProviderApplication.class, args);
+    }
+
+    @Autowired
+    private ProviderProperties providerProperties;
+
+    @RequestMapping("/metas")
+    public String meta() {
+        System.out.println(System.identityHashCode(providerProperties.getMetas()));
+        return providerProperties.getMetas().toString();
     }
 }
